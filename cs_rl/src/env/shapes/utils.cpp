366e2dcba08f1aebd6b46541a354ec38353a0573
Sergio Chouhy <sergio.chouhy@gmail.com>
2019-12-20 00:02:09 +0100

    Agregué const Vec& reference por todos lados en utils

--- cs_rl/src/env/shapes/utils.cpp
+++ cs_rl/src/env/shapes/utils.cpp
@@ -1,46 +1,46 @@
 #include "utils.h"
 
-Vec2 orthogonal(Vec2 a) 
+Vec2 orthogonal(const Vec2& a) 
 {
     return {-a.y, a.x};
 }
 
-float determinant(Vec2 a, Vec2 b) 
+float determinant(const Vec2& a, const Vec2& b) 
 {
     return a.x*b.y - a.y*b.x;
 }
 
-Vec2 normalize(Vec2 a)
+Vec2 normalize(const Vec2& a)
 {
     return glm::normalize(a);
 }
 
-float dot(Vec2 a, Vec2 b)
+float dot(const Vec2& a, const Vec2& b)
 {
     return glm::dot(a,b);
 }
 
-float distance(Vec2 a, Vec2 b)
+float distance(const Vec2& a, const Vec2& b)
 {
     return glm::distance(a,b);
 }
 
-float orientedAngle(Vec2 a, Vec2 b)
+float orientedAngle(const Vec2& a, const Vec2& b)
 {
     return glm::orientedAngle(a, b);
 }
 
-float sqrt(float a)
+float sqrt(const float& a)
 {
     return glm::sqrt(a);
 }
 
-float pow(float a, int b)
+float pow(const float& a, const int& b)
 {
     return glm::pow(a, b);
 }
 
-Vec2 rotate(Vec2 a, float angle)
+Vec2 rotate(const Vec2& a, const float& angle)
 {
     return glm::rotate(a, angle);
 }
--- cs_rl/src/env/shapes/utils.h
+++ cs_rl/src/env/shapes/utils.h
@@ -15,14 +15,14 @@
 
 typedef glm::vec2 Vec2; // Tiene que tener variables publicas .x y .y
 
-Vec2 orthogonal(Vec2 a);
-float determinant(Vec2 a, Vec2 b);
-Vec2 normalize(Vec2 a);
-float dot(Vec2 a, Vec2 b);
-float distance(Vec2 a, Vec2 b);
-float orientedAngle(Vec2 a, Vec2 b);
-float sqrt(float a);
-float pow(float a, int b);
-Vec2 rotate(Vec2 a, float angle);
+Vec2 orthogonal(const Vec2& a);
+float determinant(const Vec2& a, const Vec2& b);
+Vec2 normalize(const Vec2& a);
+float dot(const Vec2& a, const Vec2& b);
+float distance(const Vec2& a, const Vec2&b);
+float orientedAngle(const Vec2& a, const Vec2& b);
+float sqrt(const float& a);
+float pow(const float& a, const int& b);
+Vec2 rotate(const Vec2& a, const float& angle);
 
 #endif // UTILS_H
